62f3297a601ce13343ba2008ed1091ec857ea928
Brent Thorne <brentathorne@gmail.com>
2025-04-12 14:37:06 -0700

    stripe filesystem setup script

--- setstripe.sh
+++ setstripe.sh
@@ -2,5 +2,6 @@ export MY_DATA=$SCRATCH/my_data
 export HW3_DATA=/global/cfs/cdirs/mp309/cs267-spr2025/hw3-datasets
 
 mkdir -p $MY_DATA
-lfs setstripe -c 72 -S 8M $MY_DATA 
-cp -r $HW3_DATA/human-chr14-synthetic* $MY_DATA
+lfs setstripe -c 72 -S 8M $MY_DATA
+#cp -r $HW3_DATA/human-chr14-synthetic* $MY_DATA
+find $HW3_DATA -type f -name "*.txt" | xargs cp -t $MY_DATA
